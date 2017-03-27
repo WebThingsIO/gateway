5208c2063bfcb0eeefe30167e074f261f6ba6cc4
Ben Francis <ben@tola.me.uk>
2017-03-27 18:47:11 +0100

    Start on a Thing database using SQLite3

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/*
+ * MozIoT Gateway App.
+ *
+ * Back end main script.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 var express = require('express');
+var db = require('./db');
 var app = express();
 
-app.use('/things', function(request, response) {
-  var options = {
-    root: 'static'
-  };
-  response.sendFile('things.json', options);
-});
+// Open the thing database.
+db.open();
+
+// Serve Things from Things router
+var thingsRouter = require('./routes/things_router');
+app.use('/things', thingsRouter);
+
+// Serve static files from static/ directory
 app.use(express.static('static'));
 
+// Start HTTP server on port 8080
 app.listen(8080, function () {
   console.log('Listening on port 8080.');
 });
