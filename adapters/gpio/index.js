22c416afd277c2e0369aab54a2470af36259f35b
Dave Hylands <dhylands@gmail.com>
2017-05-08 12:19:57 -0700

    Fixed typo.

--- adapters/gpio/index.js
+++ adapters/gpio/index.js
@@ -1,6 +1,6 @@
 /**
  *
- * index.js - Loads the ZigBee adapter.
+ * index.js - Loads the GPIO adapter.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
