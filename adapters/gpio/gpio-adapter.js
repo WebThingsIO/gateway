bc23b85f64bed196b8187487c8b29d4b2be6e481
Dave Hylands <dhylands@gmail.com>
2017-05-10 17:57:12 -0700

    Fix GPIO failing to load on OSX

--- adapters/gpio/gpio-adapter.js
+++ adapters/gpio/gpio-adapter.js
@@ -91,29 +91,9 @@ class GpioAdapter extends Adapter {
   }
 }
 
-function loadGpioAdapters(adapterManager) {
-  var gpioConfigFilename = 'config/gpio-config.js';
-
-  // Verify that we have write permissions to /sys/class/gpio/export. Under
-  // regular linux, this file is owned by root, so the server would need to
-  // run as the root user. On the Raspberry Pi, being a member of the gpio
-  // group allows non-root access.
-  //
-  // This file won't exist on Mac/Windows.
-  try {
-    fs.accessSync('/sys/class/gpio/export', fs.constants.W_OK);
-  } catch (err) {
-    console.log('Not starting GPIO adapter - no permissions to /sys/class/gpio/export')
-    return;
-  }
-
-  if (fs.existsSync(gpioConfigFilename)) {
-    var gpioConfigs = require('../../' + gpioConfigFilename);
-    /* jshint -W031 */
-    new GpioAdapter(adapterManager, gpioConfigs);
-  } else {
-    console.log('Not starting GPIO adapter - no config file');
-  }
+function loadGpioAdapter(adapterManager, gpioConfig) {
+  /* jshint -W031 */
+  new GpioAdapter(adapterManager, gpioConfigs);
 }
 
-module.exports = loadGpioAdapters;
+module.exports = loadGpioAdapter;
