69d18616950babe519f2f0473954f79699e8168f
James Lal <jlal@mozilla.com>
2017-06-20 21:00:09 -0700

    Introduce JSONWebToken model for validating tokens.

---
+++ test/models/jsonwebtoken-test.js
@@ -0,0 +1,17 @@
+const JSONWebToken = require('../../models/jsonwebtoken');
+const {assert} = require('../common');
+
+describe('JSONWebToken', function() {
+
+  it('should be able to round trip', () => {
+    const userId = 1;
+    const {sig, token} = JSONWebToken.create(userId);
+    const subject = new JSONWebToken(token);
+
+    const {sig: sig2} = JSONWebToken.create(userId);
+
+    const result = subject.verify(sig);
+    assert(!subject .verify(sig2));
+  });
+
+});
