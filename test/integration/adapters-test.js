297e8cf8ab3344ba5443aade069347de52b18420
James Hobin <hobinjk@gmail.com>
2017-06-22 11:36:01 -0400

    Update adapters-test to new style

--- test/integration/adapters-test.js
+++ test/integration/adapters-test.js
@@ -1,11 +1,12 @@
 'use strict';
 
 /* Tell jshint about mocha globals, and  */
-/* globals it, chai, mockAdapter, server */
+/* globals it */
 
-require('../common');
+const {server, chai, mockAdapter, rp} = require('../common');
 
 var Constants = require('../../constants');
+
 
 
 it('gets all adapters', (done) => {
