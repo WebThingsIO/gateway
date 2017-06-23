332a71e9f073f8495fae099af77dd21fb77a28d9
Dave Hylands <dhylands@gmail.com>
2017-06-23 13:02:37 -0700

    Install script for Raspberry Pi

---
+++ run-app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+run_app() {
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+  echo "nvm version"
+  nvm --version
+  echo "node version"
+  node --version
+  echo "npm version"
+  npm --version
+  echo "Starting gateway ..."
+  npm start
+}
+
+run_app > ${HOME}/mozilla-iot/gateway/run-app.log 2>&1
