ce1ef76268efd59ac10ca7065115c100a4544c2a
James Hobin <hobinjk@gmail.com>
2017-06-29 10:29:17 -0700

    Replace Authentication and bcrypt with Passwords

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var bcrypt = require('bcrypt-nodejs');
+const Passwords = require('../passwords');
 
 var User = function(id, email, password, name) {
   this.id = id;
@@ -20,24 +20,9 @@ var User = function(id, email, password, name) {
 };
 
 User.generate = async function(email, rawPassword, name) {
-  // TODO: Make async.
-  const hash = bcrypt.hashSync(rawPassword);
+  const hash = await Passwords.hash(rawPassword);
   return new User(null, email, hash, name);
 }
-
-/**
- * Check stored password against provided password.
- *
- * @param {String} password Password provided.
- * @return {Boolean} True is matches, false if not.
- */
-User.prototype.checkPassword = function(password) {
-  if (bcrypt.compareSync(password, this.password)) {
-    return true;
-  } else {
-    return false;
-  }
-};
 
 /**
  * Get a JSON description for this user.
