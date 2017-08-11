eb1cee73968e748867782777b0c1c938c3a601e2
Ben Francis <ben@tola.me.uk>
2017-08-11 17:57:23 +0100

    View user settings

--- static/js/settings.js
+++ static/js/settings.js
@@ -15,73 +15,92 @@
 // eslint-disable-next-line no-unused-vars
 var SettingsScreen = {
 
-    /**
-    * Initialise Settings Screen.
-    */
-    init: function() {
-      this.menu = document.getElementById('settings-menu');
-      this.domainSettings = document.getElementById('domain-settings');
-      this.backButton = document.getElementById('settings-back-button');
-    },
+  /**
+  * Initialise Settings Screen.
+  */
+  init: function() {
+    this.menu = document.getElementById('settings-menu');
+    this.domainSettings = document.getElementById('domain-settings');
+    this.userSettings = document.getElementById('user-settings');
+    this.backButton = document.getElementById('settings-back-button');
+  },
 
-    show: function(section) {
-      if (section) {
-        this.showSection(section);
+  show: function(section) {
+    if (section) {
+      this.showSection(section);
+    } else {
+      this.showMenu();
+    }
+  },
+
+  showMenu: function() {
+    App.showMenuButton();
+    this.hideBackButton();
+    this.menu.classList.remove('hidden');
+    this.domainSettings.classList.add('hidden');
+    this.userSettings.classList.add('hidden');
+  },
+
+  hideMenu: function() {
+    this.menu.classList.add('hidden');
+  },
+
+  showBackButton: function() {
+    this.backButton.classList.remove('hidden');
+  },
+
+  hideBackButton: function() {
+    this.backButton.classList.add('hidden');
+  },
+
+  showSection: function(section) {
+    switch(section) {
+      case 'domain':
+        this.showDomainSettings();
+        break;
+      case 'users':
+        this.showUserSettings();
+        break;
+      default:
+        console.error('Tried to display undefined section');
+        return;
+    }
+    this.hideMenu();
+    App.hideMenuButton();
+    this.showBackButton();
+  },
+
+  showDomainSettings: function() {
+    this.domainSettings.classList.remove('hidden');
+    const opts = {
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+    fetch('/settings/tunnelinfo', opts).then(function (response) {
+      return response.text();
+    }).then(function (body) {
+      if (body) {
+        document.getElementById('current-domain').innerText = body;
       } else {
-        this.showMenu();
+        document.getElementById('current-domain').innerText = 'Not set.';
       }
-    },
+    });
+  },
 
-    showMenu: function() {
-      App.showMenuButton();
-      this.hideBackButton();
-      this.menu.classList.remove('hidden');
-      this.domainSettings.classList.add('hidden');
-    },
-
-    hideMenu: function() {
-      this.menu.classList.add('hidden');
-    },
-
-    showBackButton: function() {
-      this.backButton.classList.remove('hidden');
-    },
-
-    hideBackButton: function() {
-      this.backButton.classList.add('hidden');
-    },
-
-    showSection: function(section) {
-      switch(section) {
-        case 'domain':
-          this.showDomainSettings();
-          break;
-        default:
-          console.error('Tried to display undefined section');
-          return;
-      }
-      this.hideMenu();
-      App.hideMenuButton();
-      this.showBackButton();
-    },
-
-    showDomainSettings: function() {
-      this.domainSettings.classList.remove('hidden');
-      const opts = {
-        headers: {
-          'Accept': 'application/json'
-        }
-      };
-     fetch('/settings/tunnelinfo', opts)
-       .then(function (response) {
-           return response.text();
-       })
-       .then(function (body) {
-         if (body) {
-           document.getElementById('current-domain').innerText = body;
-         } else {
-           document.getElementById('current-domain').innerText = 'Not set.';
-         }
-      });
-    },
+  showUserSettings: function() {
+    this.userSettings.classList.remove('hidden');
+    window.API.getUserInfo().then(function (response) {
+     if (response.name) {
+       document.getElementById('current-user-name').innerText = response.name;
+     } else {
+       document.getElementById('current-user-name').innerText = '';
+     }
+     if (response.email) {
+       document.getElementById('current-user-email').innerText = response.email;
+     } else {
+       document.getElementById('current-user-email').innerText = '';
+     }
+    });
+  }
 };
