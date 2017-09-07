f5eb23f82f15ecf2ccf2446e27ffe9e2c471ee33
James Hobin <hobinjk@mit.edu>
2017-09-07 15:36:43 -0400

    Make CA archive utilities functions

--- tools/create-release-archives.sh
+++ tools/create-release-archives.sh
@@ -2,7 +2,13 @@
 
 # TODO: requires gateway.tar.gz to already exist
 
+createCAArchive() {
+  base_name=$1
+  digest=$(openssl dgst -sha256 $base_name.tar.gz | awk '{print $2}')
+  mv $base_name.tar.gz $base_name-$digest.tar.gz
+}
+
 tar czf node_modules.tar.gz node_modules
-./tools/create-ca-archive.sh node_modules
-./tools/create-ca-archive.sh gateway
+createCAArchive node_modules
+createCAArchive gateway
 
