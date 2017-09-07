a475f3299685f2cf37c91376106eeb7d55c251b2
James Hobin <hobinjk@gmail.com>
2017-09-07 16:37:06 +0000

    Fix CA archive extraction

--- tools/extract-ca-archive.sh
+++ tools/extract-ca-archive.sh
@@ -5,19 +5,12 @@ target=$2
 digest=${file#*-}
 digest=${digest%.tar.gz}
 base_name=${file%-*}
-echo $base_name
-echo $digest
-exit 0
 
 check_digest=$(openssl dgst -sha256 $base_name-$digest.tar.gz | awk '{print $2}')
 
 if [ "$digest" != "$check_digest" ]; then
   echo "Digest mismatch: $digest != $check_digest"
   exit -1
-fi
-
-if [ -d $base_name ]; then
-  rm -r $base_name
 fi
 
 tar xzf "$base_name-$digest.tar.gz" -C $target
