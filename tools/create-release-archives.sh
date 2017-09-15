14db4e18bce44d0f5d65c1ae96d327db1ace3ac0
James Hobin <hobinjk@gmail.com>
2017-09-15 10:30:31 -0400

    Document and improve error-checking of create-release-archives.sh

--- tools/create-release-archives.sh
+++ tools/create-release-archives.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# TODO: requires gateway.tar.gz to already exist
+# Creates the gateway-<hash>,tar.gz and node_modules-<hash>.tar.gz archives
+# which are then uploaded to the GitHub release
 
-createCAArchive() {
+if [ ! -d "node_modules" ] || [ ! -e "gateway.tar.gz" ]; then
+  echo "gateway.tar.gz and node_modules/ must be present. See https://github.com/mozilla-iot/gateway/blob/master/doc/how-to-ota.md for more information"
+  exit 1
+fi
+
+makeContentAddressedArchive() {
   base_name=$1
   digest=$(openssl dgst -sha256 $base_name.tar.gz | awk '{print $2}')
   mv $base_name.tar.gz $base_name-$digest.tar.gz
 }
 
 tar czf node_modules.tar.gz node_modules
-createCAArchive node_modules
-createCAArchive gateway
+makeContentAddressedArchive node_modules
+makeContentAddressedArchive gateway
 
