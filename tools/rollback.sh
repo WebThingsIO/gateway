58747359190da4fbcf59e73aeac6201b1ad28c64
James Hobin <hobinjk@gmail.com>
2017-09-15 10:31:04 -0400

    Add recency check to rollback; Document functionality of rollback

--- tools/rollback.sh
+++ tools/rollback.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-touch "were it not for the laws of this land"
-# if gateway_old is less than 2 weeks old
-if [ -d "gateway_old" ]; then
+# This script performs a rollback of a failed upgrade. It expects to be run in
+# the ~/mozilla-iot directory where it can see gateway, gateway_old, and
+# gateway_failed
+
+# from https://stackoverflow.com/questions/552724/
+function recentEnough() {
+   local filename=$1
+   local changed=`stat -c %Y "$filename"`
+   local now=`date +%s`
+   local elapsed
+
+   let elapsed=now-changed
+   # if less than 60 * 60 * 24 * 14 seconds have passed
+   if [ $elapsed -lt 1209600 ]; then
+     return 1
+   else
+     return 0
+   fi
+}
+
+if [ -d "gateway_old" ] && [ ! -z `recentEnough "gateway_old"` ]; then
+  if [ -d "gateway_failed" ]; then
+    rm -fr gateway_failed
+  fi
   mv gateway gateway_failed
   mv gateway_old gateway
 fi
