bc1ddf96bb5c3204c4dca7b9681928573e29bee9
Andre Natal <anatal@gmail.com>
2017-09-23 11:44:57 -0700

    Implementing speech commands interface and integration with intent REST API

--- static/js/speech.js
+++ static/js/speech.js
@@ -31,7 +31,11 @@ var Speech = {
         if (this.listening) {
           this.listening = false;
           this.stm.stop();
+          document.getElementById('speech-button').style.backgroundImage =
+              `url('/images/microphone.svg')`;
         } else {
+          document.getElementById('speech-button').style.backgroundImage =
+              `url('/images/microphone-active.svg')`;
           this.stm.listen();
           this.listening = true;
         }
@@ -42,19 +46,154 @@ var Speech = {
      * @param msg
      */
     listener: function(msg) {
-        if (msg.state == 'result') {
+        if (msg.state === 'result') {
+          var x = document.getElementById('snackbar');
+          const displayNotification = function(msg, audio) {
+            x.innerHTML = msg;
+            x.className = 'show';
+            setTimeout(function(){ x.className =
+                x.className.replace('show', ''); }, 3000);
+            new Audio(`/audio/${audio}.mp3`).play();
+          }
+          document.getElementById('stm-levels').hidden = true;
+          document.getElementById('speech-button').style.backgroundImage =
+              `url('/images/microphone.svg')`;
           // sort results to get the one with the highest confidence
           var results = msg.data.sort(function(a, b) {
-              return a.confidence - b.confidence;
+              return b.confidence - a.confidence;
           });
-          // call the intent rest api with the sentence we decoded
-          // I want to keep the output here until we have the toast and
-          // the communication with the Intent API landed and working
-          // so I could debug the results
+          if (results.length < 1) {
+              displayNotification(
+                  'Sorry, we couldn\'t understand your command.', 'failure');
+              console.log('Error: (results.length <= 1)');
+              return;
+          }
           console.log(results[0].text, results[0].confidence);
+          var opts = {
+            method: 'POST',
+            cache: 'default',
+            body: `{"text":"${results[0].text}"}`,
+            headers: {
+                'Authorization': `Bearer ${window.API.jwt}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+          };
+
+          let cmdError;
+          fetch('/commands', opts).then(function(response) {
+             if(!response.ok) {
+                 cmdError = true;
+             }
+             return response.json();
+          }).then(function() {
+              if (cmdError) {
+                  displayNotification(
+                      'Sorry, the command wasn\'t found.', 'failure');
+              } else {
+                  displayNotification(
+                      'The command was successfully executed.', 'success');
+              }
+          }).catch(function() {
+              displayNotification(
+                  'Sorry, we found a problem processing your command.',
+                  'failure');
+          });
         }
-        else if (msg.state == 'ready') {
+        else if (msg.state === 'ready') {
           this.listening = false;
+        } else if (msg.state === 'listening') {
+            let mediaStream = this.stm.getmediaStream();
+            if (mediaStream) {
+                document.getElementById('stm-levels').hidden = false;
+
+                // Build the WebAudio graph we'll be using
+                let audioContext = new AudioContext();
+                let sourceNode = audioContext.
+                    createMediaStreamSource(mediaStream);
+                let analyzerNode = audioContext.createAnalyser();
+                let outputNode = audioContext.createMediaStreamDestination();
+
+                // make sure we're doing mono everywhere
+                sourceNode.channelCount = 1;
+                analyzerNode.channelCount = 1;
+                outputNode.channelCount = 1;
+
+                // connect the nodes together
+                sourceNode.connect(analyzerNode);
+                analyzerNode.connect(outputNode);
+
+                this.visualize(analyzerNode);
+            }
         }
+    },
+
+    // Helper to handle background visualization
+    visualize: function(analyzerNode) {
+        let api = this;
+        const MIN_DB_LEVEL = -85; // The dB level that's 0 in the display
+        const MAX_DB_LEVEL = -30; // The dB level that's 100% in the  display
+
+        // Set up the analyzer node, and allocate an array for its data
+        // FFT size 64 gives us 32 bins. But those bins hold frequencies up to
+        // 22kHz or more, and we only care about visualizing lower frequencies
+        // which is where most human voice lies, so we use fewer bins
+        analyzerNode.fftSize = 64;
+        const frequencyBins = new Float32Array(14);
+
+        // Clear the canvas
+        const levels = document.getElementById('stm-levels');
+        const xPos = levels.width/2 + 78;
+        const yPos = levels.height/2 - 40;
+        const context = levels.getContext('2d');
+        context.clearRect(0, 0, levels.width, levels.height);
+
+        if (levels.hidden) {
+            return; // If we've been hidden, return right away
+        }
+
+        // Get the FFT data
+        analyzerNode.getFloatFrequencyData(frequencyBins);
+
+        // Display it as a barchart.
+        // Drop bottom few bins, since they are often misleadingly high
+        const skip = 2;
+        const n = frequencyBins.length - skip;
+        const dbRange = MAX_DB_LEVEL - MIN_DB_LEVEL;
+
+        // Loop through the values and draw the bars
+        context.strokeStyle = '#d1d2d3';
+
+        for (let i = 0; i < n; i++) {
+            const value = frequencyBins[i + skip];
+            const diameter =
+                ((levels.height * (value - MIN_DB_LEVEL) / dbRange) * 3) - 70;
+            if (diameter < 0) {
+                continue;
+            }
+            //console.log(xPos, yPos, diameter - 10, diameter/2 - 10);
+            // Display a bar for this value.
+            var alpha = diameter/500;
+            if(alpha > .2) alpha = .2;
+            else if (alpha < .1) alpha = .1;
+
+            context.lineWidth = alpha*alpha*150;
+            context.globalAlpha = alpha*alpha*5;
+            context.beginPath();
+            context.ellipse(
+                xPos,
+                yPos,
+                diameter/2,
+                diameter/4,
+                0,
+                0,
+                2 * Math.PI
+            );
+            if(diameter > 90 && diameter < 360) context.stroke();
+        }
+        // Update the visualization the next time we can
+        requestAnimationFrame(function() {
+            api.visualize(analyzerNode);
+        });
     }
 }
