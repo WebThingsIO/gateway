7e5af691cce706413a12397c3c39227826449cbf
James Hobin <hobinjk@gmail.com>
2017-10-05 14:15:49 -0400

    Use Database settings API in Settings model

--- src/models/settings.js
+++ src/models/settings.js
@@ -1,7 +1,7 @@
 /**
  * Settings Model.
  *
- * Manages the getting and setting of settings using node-persist.
+ * Manages the getting and setting of settings
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -10,19 +10,9 @@
 
 'use strict';
 
-const config = require('config');
-const storage = require('node-persist');
+const Database = require('../db');
 
 const Settings = {
-
-  /**
-   * Initialise setting storage.
-   */
-  init: function() {
-    return storage.init({
-      dir: config.get('settings.directory')
-    });
-  },
 
   /**
    * Get a setting.
@@ -30,17 +20,9 @@ const Settings = {
    * @param {String} key Key of setting to get.
    */
   get: function(key) {
-    return new Promise((resolve, reject) => {
-      this.init()
-      .then(function() {
-        storage.getItem(key)
-        .then(function(value) {
-          resolve(value);
-        }).catch(function(e) {
-          console.error('Failed to get ' + key);
-          reject(e);
-        })
-      });
+    return Database.getSetting(key).catch(function(e) {
+      console.error('Failed to get ' + key);
+      throw e;
     });
   },
 
@@ -51,21 +33,12 @@ const Settings = {
    * @param value Value to set key to.
    */
   set: function(key, value) {
-    return new Promise((resolve, reject) => {
-      this.init()
-      .then(function() {
-        storage.setItem(key, value)
-        .then(function() {
-          return storage.getItem(key);
-        })
-        .then(function(returnedValue) {
-          console.log('Set ' + key + ' to ' + value);
-          resolve(returnedValue);
-        }).catch(function(e) {
-          console.error('Failed to set ' + key + ' to ' + value);
-          reject(e);
-        })
-      });
+    return Database.setSetting(key, value).then(function() {
+      console.log('Set ' + key + ' to ' + value);
+      return value;
+    }).catch(function(e) {
+      console.error('Failed to set ' + key + ' to ' + value);
+      throw e;
     });
   }
 };
