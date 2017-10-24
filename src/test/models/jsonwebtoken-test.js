cb6c48af56d6c0b49264c4dd470a0c404b1b252b
James Hobin <hobinjk@gmail.com>
2017-10-24 11:48:25 -0700

    Add tests of JWT kid and alg values
    Test that our JWT library resists a series of potential attacks against
    its verifyJWT method.

--- src/test/models/jsonwebtoken-test.js
+++ src/test/models/jsonwebtoken-test.js
@@ -1,4 +1,7 @@
 const JSONWebToken = require('../../models/jsonwebtoken');
+const uuid = require('uuid');
+const jwt = require('jsonwebtoken');
+const ec = require('../../ec-crypto');
 
 describe('JSONWebToken', function() {
 
@@ -11,6 +14,32 @@ describe('JSONWebToken', function() {
 
     expect(subject.verify(sig)).toBeTruthy();
     expect(subject.verify(sig2)).toEqual(false);
+  });
+
+  it('should fail to verify with a missing key id', async () => {
+    const pair = ec.generateKeyPair();
+    const sig = jwt.sign({}, pair.private, {
+      algorithm: ec.JWT_ALGORITHM
+    });
+    expect(await JSONWebToken.verifyJWT(sig)).toEqual(false);
+  });
+
+  it('should fail to verify with an incorrect key id', async () => {
+    const pair = ec.generateKeyPair();
+    const sig = jwt.sign({}, pair.private, {
+      algorithm: ec.JWT_ALGORITHM,
+      keyid: 'tomato'
+    });
+    expect(await JSONWebToken.verifyJWT(sig)).toEqual(false);
+  });
+
+  it('should fail to verify a JWT with the "none" alg', async () => {
+    const pair = ec.generateKeyPair();
+    const sig = jwt.sign({}, pair.private, {
+      algorithm: 'none',
+      keyid: uuid.v4()
+    });
+    expect(await JSONWebToken.verifyJWT(sig)).toEqual(false);
   });
 
 });
