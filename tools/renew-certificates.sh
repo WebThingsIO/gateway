5bfb1a37f9e2e9f51eed6b8071aeab41ea9f212e
Mike Stegeman <mrstegeman@gmail.com>
2017-11-06 07:47:29 -0700

    Replace --deploy-hook with --renew-hook in cert renewal script.

--- tools/renew-certificates.sh
+++ tools/renew-certificates.sh
@@ -2,10 +2,13 @@
 
 moziot_dir="/home/pi/mozilla-iot"
 
+# NOTE: --renew-hook is deprecated and has been replaced by --deploy-hook.
+#       However, the version of certbot in raspbian stretch doesn't have
+#       --deploy-hook support, so we're sticking with the old option for now.
 certbot renew \
     --config-dir "$moziot_dir/etc" \
     --logs-dir "$moziot_dir/var/log" \
     --work-dir "$moziot_dir/var/lib" \
-    --deploy-hook "$moziot_dir/gateway/tools/deploy-certificates.sh"
+    --renew-hook "$moziot_dir/gateway/tools/deploy-certificates.sh"
 
 chown -R pi:pi "$moziot_dir/etc" "$moziot_dir/var"
