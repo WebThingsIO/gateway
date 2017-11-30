7b4b3b62e2b0e2c6a4b1b1379b7fb626e1aec28c
Andre Natal <anatal@gmail.com>
2017-11-30 13:01:49 -0800

    Fix comments

--- image/prepare-base-root.sh
+++ image/prepare-base-root.sh
@@ -8,18 +8,14 @@ export NVM_DIR="$HOME/.nvm"
 nvm install v7.10.1
 nvm use v7.10.1
 
-# baixa o tarball do codigo
+# download and install the code
 wget https://github.com/mozilla-iot/gateway-wifi-setup/archive/master.zip
-
-# descompacta e instala
 unzip master.zip -d .
 mv gateway-wifi-setup-master/ gateway-wifi-setup/
 cd gateway-wifi-setup/
 npm install
-
-# transforma o run.sh em executavel
 chmod +x run.sh
 
-# copia o servico para o folder de instalacao com o boot de start e type=oneshot
+# and install the systemd service
 cp config/mozilla-gateway-wifi-setup.service /etc/systemd/system/mozilla-gateway-wifi-setup.service
 sudo systemctl enable mozilla-gateway-wifi-setup.service
