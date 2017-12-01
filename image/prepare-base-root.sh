b71939f07a614411b7a703322f333af5d4ceb5ef
Andre Natal <andrenatal@users.noreply.github.com>
2017-12-01 12:02:23 -0800

    Revert "Install hostapd and dnsmasq config files"
    This reverts commit f46e29b27af8607eee0f117203e59dd8f4ec9046.

--- image/prepare-base-root.sh
+++ image/prepare-base-root.sh
@@ -16,17 +16,6 @@ cd gateway-wifi-setup/
 npm install
 chmod +x run.sh
 
-# install and enable the systemd service
+# and install the systemd service
 cp config/mozilla-gateway-wifi-setup.service /etc/systemd/system/mozilla-gateway-wifi-setup.service
-systemctl enable mozilla-gateway-wifi-setup.service
-
-# disable hostapd and dnsmasq auto start
-systemctl disable hostapd.service
-systemctl disable dnsmasq.service
-
-# install hostapd and dnsmasq config files 
-cp config/hostapd.conf /etc/hostapd/hostapd.conf
-cp config/dnsmasq.conf /etc/dnsmasq.conf
-sudo sh -c 'cat >> /etc/default/hostapd' << END
-DAEMON_CONF="/etc/hostapd/hostapd.conf"
-END
+sudo systemctl enable mozilla-gateway-wifi-setup.service
