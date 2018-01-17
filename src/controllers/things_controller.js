d59166d9c1103d0b061f28414f1d976eaaa5b2c8
Mike Stegeman <mrstegeman@gmail.com>
2018-01-17 07:52:50 -0700

    Fix crash when sending data to closed websocket.
    I hit a case where a websocket error event was thrown without a
    subsequent close event. Handle that.

--- src/controllers/things_controller.js
+++ src/controllers/things_controller.js
@@ -211,9 +211,13 @@ ThingsController.ws('/:thingId/', function(websocket, request) {
   AddonManager.on(Constants.PROPERTY_CHANGED, onPropertyChanged);
   Actions.on(Constants.ACTION_STATUS, onActionStatus);
 
+  websocket.on('error', function() {
+    AddonManager.removeListener(Constants.PROPERTY_CHANGED, onPropertyChanged);
+    Actions.removeListener(Constants.ACTION_STATUS, onActionStatus);
+  });
+
   websocket.on('close', function() {
-    AddonManager.removeListener(Constants.PROPERTY_CHANGED,
-                                onPropertyChanged);
+    AddonManager.removeListener(Constants.PROPERTY_CHANGED, onPropertyChanged);
     Actions.removeListener(Constants.ACTION_STATUS, onActionStatus);
   });
 
