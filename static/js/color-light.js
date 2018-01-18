52791030ba76eb7e3e5b0be8d7b62fa6e06dc5da
James Hobin <hobinjk@gmail.com>
2018-01-18 15:23:08 -0500

    Add ON/OFF text; Darken light based on color selected

--- static/js/color-light.js
+++ static/js/color-light.js
@@ -30,6 +30,7 @@ function ColorLight(description, format) {
 
   this.updateStatus();
   this.colorLight = this.element.querySelector('.color-light');
+  this.colorLightLabel = this.element.querySelector('.color-light-label');
   this.colorLightIconPath =
     this.element.querySelector('.color-light-icon-path');
 
@@ -54,53 +55,57 @@ ColorLight.prototype = Object.create(OnOffSwitch.prototype);
 ColorLight.prototype.iconView = function() {
   let color = this.properties.color;
   return `<div class="color-light" style="background: ${color};">
-    <svg
-       xmlns:dc="http://purl.org/dc/elements/1.1/"
-       xmlns:cc="http://creativecommons.org/ns#"
-       xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-       xmlns:svg="http://www.w3.org/2000/svg"
-       xmlns="http://www.w3.org/2000/svg"
-       version="1.1"
-       viewBox="0 0 64 64"
-       height="64"
-       width="64"
-       class="color-light-icon">
-      <defs
-         id="defs16980" />
-      <metadata
-         id="metadata16983">
-        <rdf:RDF>
-          <cc:Work
-             rdf:about="">
-            <dc:format>image/svg+xml</dc:format>
-            <dc:type
-               rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-            <dc:title></dc:title>
-          </cc:Work>
-        </rdf:RDF>
-      </metadata>
-      <g transform="translate(0,-988.36216)">
-        <path
-           d="m 41.6997,1041.6985 c 0,1.0723 -0.8727,1.9367 -1.9366,1.9367 l
-           -15.5179,0 c -1.0722,0 -1.9366,-0.8727 -1.9366,-1.9367 0,-1.0722
-           0.8727,-1.9366 1.9366,-1.9366 l 15.5179,0 c 1.0639,-0.01
-           1.9366,0.8644 1.9366,1.9366 z m -1.9449,2.9091 -15.5096,0 c
-           -1.28,0 -2.2608,1.2302 -1.8369,2.5683 0.2577,0.8063 1.0722,1.305
-           1.92,1.305 l 0.033,0 c 1.1221,0 2.1444,0.6317 2.6431,1.6374 l
-           0.017,0.041 c 0.6732,1.3465 2.053,2.2026 3.5657,2.2026 l 2.826,0 c
-           1.5127,0 2.8925,-0.8561 3.5657,-2.2026 l 0.017,-0.041 c
-           0.4987,-1.0057 1.5294,-1.6374 2.6432,-1.6374 l 0.033,0 c 0.8478,0
-           1.6623,-0.4987 1.92,-1.305 0.4322,-1.3381 -0.5569,-2.5683
-           -1.8369,-2.5683 z m 1.9449,-7.7631 c 0,1.0722 -0.8727,1.9366
-           -1.9366,1.9366 l -15.5179,0 c -1.0722,0 -1.9366,-0.8727
-           -1.9366,-1.9366 0,-1.0306 0.8062,-1.8701 1.8285,-1.9283
-           -1.2135,-10.9132 -12.5008,-13.3403 -12.5008,-26.19 0,-11.24577
-           9.118,-20.36377 20.3637,-20.36377 11.2457,0 20.3637,9.118
-           20.3637,20.36377 0,12.8497 -11.2873,15.2768 -12.4925,26.19
-           1.014,0.058 1.8285,0.8977 1.8285,1.9283 z"
-           class="color-light-icon-path"/>
-      </g>
-    </svg>
+    <div class="color-light-icon">
+      <svg
+         xmlns:dc="http://purl.org/dc/elements/1.1/"
+         xmlns:cc="http://creativecommons.org/ns#"
+         xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+         xmlns:svg="http://www.w3.org/2000/svg"
+         xmlns="http://www.w3.org/2000/svg"
+         version="1.1"
+         viewBox="0 0 64 64"
+         height="64"
+         width="64">
+        <defs
+           id="defs16980" />
+        <metadata
+           id="metadata16983">
+          <rdf:RDF>
+            <cc:Work
+               rdf:about="">
+              <dc:format>image/svg+xml</dc:format>
+              <dc:type
+                 rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+              <dc:title></dc:title>
+            </cc:Work>
+          </rdf:RDF>
+        </metadata>
+        <g transform="translate(0,-988.36216)">
+          <path
+             d="m 41.6997,1041.6985 c 0,1.0723 -0.8727,1.9367 -1.9366,1.9367 l
+             -15.5179,0 c -1.0722,0 -1.9366,-0.8727 -1.9366,-1.9367 0,-1.0722
+             0.8727,-1.9366 1.9366,-1.9366 l 15.5179,0 c 1.0639,-0.01
+             1.9366,0.8644 1.9366,1.9366 z m -1.9449,2.9091 -15.5096,0 c
+             -1.28,0 -2.2608,1.2302 -1.8369,2.5683 0.2577,0.8063 1.0722,1.305
+             1.92,1.305 l 0.033,0 c 1.1221,0 2.1444,0.6317 2.6431,1.6374 l
+             0.017,0.041 c 0.6732,1.3465 2.053,2.2026 3.5657,2.2026 l 2.826,0 c
+             1.5127,0 2.8925,-0.8561 3.5657,-2.2026 l 0.017,-0.041 c
+             0.4987,-1.0057 1.5294,-1.6374 2.6432,-1.6374 l 0.033,0 c 0.8478,0
+             1.6623,-0.4987 1.92,-1.305 0.4322,-1.3381 -0.5569,-2.5683
+             -1.8369,-2.5683 z m 1.9449,-7.7631 c 0,1.0722 -0.8727,1.9366
+             -1.9366,1.9366 l -15.5179,0 c -1.0722,0 -1.9366,-0.8727
+             -1.9366,-1.9366 0,-1.0306 0.8062,-1.8701 1.8285,-1.9283
+             -1.2135,-10.9132 -12.5008,-13.3403 -12.5008,-26.19 0,-11.24577
+             9.118,-20.36377 20.3637,-20.36377 11.2457,0 20.3637,9.118
+             20.3637,20.36377 0,12.8497 -11.2873,15.2768 -12.4925,26.19
+             1.014,0.058 1.8285,0.8977 1.8285,1.9283 z"
+             class="color-light-icon-path"/>
+        </g>
+      </svg>
+      <div class="color-light-label">
+        ON
+      </div>
+    </div>
   </div>`;
 };
 
@@ -210,8 +215,12 @@ ColorLight.prototype.updateOn = function(on) {
   if (on === null) {
     return;
   }
+
+  let onoff = on ? 'on' : 'off';
+  this.colorLightLabel.textContent = onoff;
+
   if (this.onOffLabel) {
-    this.onOffLabel.textContent = on ? 'on' : 'off';
+    this.onOffLabel.textContent = onoff;
     this.onOffSwitch.checked = on;
   }
 
@@ -224,6 +233,9 @@ ColorLight.prototype.updateOn = function(on) {
 
 ColorLight.prototype.updateColor = function(color) {
   this.properties.color = color;
+  if (!this.colorLight) {
+    return;
+  }
   this.colorLight.style.background = color;
   if (this.colorInput) {
     this.colorInput.value = color;
@@ -235,9 +247,9 @@ ColorLight.prototype.updateColor = function(color) {
 
   // From https://stackoverflow.com/questions/3942878/
   if (r * 0.299 + g * 0.587 + b * 0.114 > 186) {
-    this.colorLightIconPath.style.stroke = '#666666';
+    this.colorLight.classList.add('bright-color');
   } else {
-    this.colorLightIconPath.style.stroke = '#ffffff';
+    this.colorLight.classList.remove('bright-color');
   }
 };
 
