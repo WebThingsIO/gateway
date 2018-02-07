152feae5601e5e2a989d6366c3da8fbf00b2800a
Swaagie <info@martijnswaagman.nl>
2018-02-08 00:06:18 +0100

    [tiny] update prepare root script to use --lts

--- image/prepare-base-root.sh
+++ image/prepare-base-root.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+NVM_VERSION=v0.33.8
+NODE_VERSION="--lts"
+
 # first we install nvm
 cd $HOME
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install v7.10.1
-nvm use v7.10.1
+nvm install ${NODE_VERSION}
+nvm use ${NODE_VERSION}
 
 # download and install the code
 wget https://github.com/mozilla-iot/gateway-wifi-setup/archive/master.zip
