8b496d21917e5dc5959a552f9eddb41ac80f1637
James Hobin <hobinjk@gmail.com>
2018-03-30 11:40:57 -0400

    Upgrade gateway-addon Python package

--- tools/post-upgrade.sh
+++ tools/post-upgrade.sh
@@ -7,3 +7,8 @@ nvm install ${NODE_VERSION}
 nvm use ${NODE_VERSION}
 # always use most recent version (lts)
 nvm alias default node
+
+# Upgrade gateway-addon Python package
+_url="git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon"
+sudo pip2 install -U "$_url"
+sudo pip3 install -U "$_url"
