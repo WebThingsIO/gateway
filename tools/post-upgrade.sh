729e790fda8cc4e7322730c657e99bea2117c04b
Michael Stegeman <michael@stegeman.me>
2018-04-11 14:22:43 -0600

    Allow node to start/stop BLE advertising.
    See: https://github.com/noble/noble#running-on-linux
    
    Fixes: mozilla-iot/thing-url-adapter#28

--- tools/post-upgrade.sh
+++ tools/post-upgrade.sh
@@ -11,6 +11,9 @@ nvm use ${NODE_VERSION}
 # always use most recent version (lts)
 nvm alias default node
 
+# Allow node to use the Bluetooth adapter
+sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
+
 # Upgrade gateway-addon Python package
 _url="git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon"
 sudo pip2 install -U "$_url"
