c6aef36f72bee11cd106a7912dd826481b9f8bba
Ziran Sun <ziran.sun@samsung.com>
2018-04-16 09:13:58 -0600

    Add shell script for issuing JWT Token.
    Fixes #778
    
    Signed-off-by: Ziran Sun <ziran.sun@samsung.com>

---
+++ tools/issue-token.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Issues JWT token signature with JSONWebToken APIs.
+
+# We need ts-node to handle TypeScript scripts.
+# Check if the required npm module is installed
+# and install the module if not.
+function npm_package_is_installed {
+  local installed_=1
+  # Set return value to 0 if not installed
+  npm list -g | grep $1 >/dev/null 2>&1 || { local installed_=0; }
+  if [ $installed_ == 0 ]; then
+    npm install -g $1
+  else
+    echo "Required module $1 is already installed"
+  fi
+}
+
+npm_package_is_installed ts-node
+npm_package_is_installed typescript
+
+// check if config directory exists
+if [[ -d config && -L config ]] ; then
+  echo "symbolic config dir exists!"
+else
+  ln -s ../config
+fi
+
+ts-node -O '{"module": "commonjs"}' issue-token.js
