160757eab723d3ebb029661a13a5c870f6157b12
sogaani <soga.ani@gmail.com>
2018-04-24 23:54:44 +0900

    add thing-view test with onOffSwitch

--- src/test/jsdom-common.js
+++ src/test/jsdom-common.js
@@ -2,13 +2,50 @@
 
 const sinon = require('sinon');
 const {JSDOM} = require('jsdom');
+const {server, chai} = require('./common');
+const {
+  TEST_USER,
+  createUser,
+} = require('./user');
+const {URL} = require('url');
+const {w3cwebsocket} = require('websocket');
 
 // Setup the jsdom environment
 const {document} =
-(new JSDOM('<!doctype html><html><body></body></html>')).window;
+(new JSDOM('<!doctype html><html><body></body></html>',
+           {url: 'https://localhost:4443/'})).window;
 global.document = document;
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
+global.URL = URL;
+// Since a test server dose not listen port, WebSocket always fails.
+global.WebSocket = w3cwebsocket;
+global.fetch = function(url, option) {
+  const {body, headers} = option;
+  const method = option.method ? option.method.toLowerCase() : 'get';
+
+  return new Promise((resolve, reject) => {
+    let request = chai.request(server)[method](url.pathname);
+    Object.keys(headers).forEach(function(key) {
+      request.set(key, headers[key]);
+    });
+
+    if (body) {
+      try {
+        request = request.send(JSON.parse(body));
+      } catch (err) {
+        reject(err);
+      }
+    }
+
+    request.end((_err, res) => {
+      res.json = () => {
+        return Promise.resolve(res.body);
+      };
+      resolve(res);
+    });
+  });
+};
 
 const strage = {};
 global.localStorage = {
@@ -34,7 +71,10 @@ expect.extend({
   },
 });
 
-beforeEach(() => {
+beforeEach(async () => {
+  const jwt = await createUser(server, TEST_USER);
+  global.localStorage.setItem('jwt', jwt);
+  global.window.API.jwt = jwt;
   global.sandbox = sinon.sandbox.create();
 });
 
