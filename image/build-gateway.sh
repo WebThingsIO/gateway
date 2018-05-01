61d895225e5a070189582413b118a755952beb7c
sogaani <soga.ani@gmail.com>
2018-05-01 23:14:47 +0900

    update cross compile scripts

--- image/build-gateway.sh
+++ image/build-gateway.sh
@@ -2,18 +2,21 @@
 #
 # This script is expected to run inside a docker container which has the Raspberry Pi toolchain
 # and required prerequisites:
-#   - pkg-config
-#   - libusb-1.0-0-dev
 #   - libudev-dev
+#   - libpng-dev
+#   - autoconf
+#   - curl
+#   - pkg-config
+#   - python
+#   - python2.7
+#   - git
+#   - build-essential
 #
 # /build corresponds to the current directory when the docker container is run and it is expected
 # that the following directory structure has been setup:
 #
 #   - /build/Open-ZWave/open-zwave  - git repository containing the desired version of OpenZWave
 #   - /build/gateway                - git repository containing the gateway software
-
-NVM_VERSION="v0.33.8"
-NODE_VERSION="--lts"
 
 GATEWAY=gateway
 OPEN_ZWAVE=OpenZWave/open-zwave
@@ -58,28 +61,9 @@ INSTALL_OPENZWAVE="PREFIX=/usr make  -C ${OPEN_ZWAVE} CROSS_COMPILE=arm-linux-gn
 sudo ${INSTALL_OPENZWAVE}
 sudo DESTDIR=${SYSROOT} ${INSTALL_OPENZWAVE}
 
-# Setup Cross compiler vars which are used by node-gyp when building native node modules.
-OPTS="--sysroot=${SYSROOT}"
-export CC="arm-linux-gnueabihf-gcc ${OPTS}"
-export CXX="arm-linux-gnueabihf-g++ ${OPTS}"
-
-# Install and configure nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash
-# The following 2 lines are installed into ~/.bashrc by the above,
-# but on the RPi, sourcing ~/.bashrc winds up being a no-op (when sourced
-# from a script), so we just run it here.
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install ${NODE_VERSION}
-nvm use ${NODE_VERSION}
-
-# Allow node to use the Bluetooth adapter
-sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
-
-npm install -g yarn
-
-# Build the node modules, cross compiling any native code.
-(cd ${GATEWAY}; yarn --ignore-scripts; npm rebuild --arch=${ARCH} --target_arch=arm)
+# Build the node modules, compiling any native code with arm emulator.
+export RPXC_UID=builder
+rpdo "cd ${GATEWAY} && source /home/builder/.nvm/nvm.sh && yarn"
 
 set -x
 
