3a3876db7c3ab071fc891f79f7655abf606f9c13
Michael Stegeman <michael@stegeman.me>
2018-05-13 13:53:43 -0600

    Fix certbot configs after moving them. (#1004)
    Fixes #1003

--- tools/renew-certificates.sh
+++ tools/renew-certificates.sh
@@ -2,6 +2,11 @@
 
 MOZIOT_HOME="${MOZIOT_HOME:=/home/pi/.mozilla-iot}"
 
+for file in $(find "${MOZIOT_HOME}/etc/renewal" -type f -name '*.conf'); do
+    sed -i -e 's_/mozilla-iot/etc_/.mozilla-iot/etc_g' "${file}"
+    sed -i -e 's_/mozilla-iot/var_/.mozilla-iot/var_g' "${file}"
+done
+
 # NOTE: --renew-hook is deprecated and has been replaced by --deploy-hook.
 #       However, the version of certbot in raspbian stretch doesn't have
 #       --deploy-hook support, so we're sticking with the old option for now.
