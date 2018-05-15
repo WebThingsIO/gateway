2089fe6002caccc6e97dd664a796e977ea0706ab
Michael Stegeman <michael@stegeman.me>
2018-05-15 09:13:19 -0600

    Disable OCSP stapling when renewing certs.
    Fixes #1008

--- tools/renew-certificates.sh
+++ tools/renew-certificates.sh
@@ -5,6 +5,9 @@ MOZIOT_HOME="${MOZIOT_HOME:=/home/pi/.mozilla-iot}"
 for file in $(find "${MOZIOT_HOME}/etc/renewal" -type f -name '*.conf'); do
     sed -i -e 's_/mozilla-iot/etc_/.mozilla-iot/etc_g' "${file}"
     sed -i -e 's_/mozilla-iot/var_/.mozilla-iot/var_g' "${file}"
+
+    # Disable OCSP stapling
+    sed -i -e 's/^must_staple.*/must_staple = False/' "${file}"
 done
 
 # NOTE: --renew-hook is deprecated and has been replaced by --deploy-hook.
