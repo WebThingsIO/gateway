88d5c0e2461f230280f6c2eeb1765635f742a270
Ben Francis <ben@tola.me.uk>
2018-06-01 15:10:45 +0100

    Change default hostname to not conflict with avahi

--- config/default.js
+++ config/default.js
@@ -43,7 +43,7 @@ module.exports = {
         localControl: {
           mdnsServiceType: "http",
           mdnsServiceName: "Mozilla IoT Gateway",
-          mdnsServiceDomain: "gateway",
+          mdnsServiceDomain: "mygateway",
 
           mdnsTxt: {
             desc: "Web of Things gateway",
