8612208be38b90d3bcda9963914ac610c182fb3e
Michael Stegeman <michael@stegeman.me>
2018-07-09 10:38:31 -0600

    Use Avahi for mDNS on Raspberry Pi.
    Fixes #1055

--- src/test/common.js
+++ src/test/common.js
@@ -13,7 +13,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const Database = require('../db');
 const Actions = require('../models/actions');
-const mDNSserver = require('../service_discovery_setup');
+const mDNSserver = require('../mdns-server');
 const Things = require('../models/things');
 const UserProfile = require('../user-profile');
 const e2p = require('event-to-promise');
@@ -126,7 +126,7 @@ afterAll(async () => {
   await addonManager.unloadAddons();
   server.close();
   httpServer.close();
-  mDNSserver.server.startService(false);
+  mDNSserver.server.setState(false);
   await Promise.all([
     e2p(server, 'close'),
     e2p(httpServer, 'close'),
