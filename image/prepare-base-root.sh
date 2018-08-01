48f444a9497d020d927d389a6bb3c34d8b61efbc
James Hobin <hobinjk@gmail.com>
2018-08-01 19:52:54 -0400

    Actually put the right path; Remove nvm

--- image/prepare-base-root.sh
+++ image/prepare-base-root.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -e
 
-NVM_VERSION="v0.33.8"
-NODE_VERSION="--lts=carbon"
-
-# first we install nvm
-cd $HOME
-curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install ${NODE_VERSION}
-nvm use ${NODE_VERSION}
-
 # Allow node and python3 to use the Bluetooth adapter
 setcap cap_net_raw+eip $(eval readlink -f `which node`)
 setcap cap_net_raw+eip $(eval readlink -f `which python3`)
@@ -20,6 +9,6 @@ systemctl disable hostapd.service
 systemctl disable dnsmasq.service
 
 # install hostapd and dnsmasq config files
-cp $HOME/mozilla-iot/gateway/image/config/hostapd.conf /etc/hostapd/hostapd.conf
-cp $HOME/mozilla-iot/gateway/image/config/dnsmasq.conf /etc/dnsmasq.conf
+cp ~pi/mozilla-iot/gateway/image/config/hostapd.conf /etc/hostapd/hostapd.conf
+cp ~pi/mozilla-iot/gateway/image/config/dnsmasq.conf /etc/dnsmasq.conf
 echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> /etc/default/hostapd
