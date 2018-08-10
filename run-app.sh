e94fb521aebb1453d546f0263c7e19dd117f121e
Philippe Coval <p.coval@samsung.com>
2018-08-10 17:42:38 +0200

    build: Support gateway-docker, quit on error
    To avoid unexpected behaviours, quit on error (set -e)
    
    Change-Id: Ifbf6401b66afd71472f1b953a7aa4b57c0ecfd3f
    Relate-to: https://github.com/mozilla-iot/gateway-docker/pull/9
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- run-app.sh
+++ run-app.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 set -x
 
 MOZIOT_HOME="${MOZIOT_HOME:=${HOME}/.mozilla-iot}"
+args=""
 
 is_docker_container() {
   if [ -f /.dockerenv ]; then
@@ -14,8 +16,8 @@ is_docker_container() {
 
   return 1
 }
-
 if ! is_docker_container; then
+  args="$args --check-wifi"
   if [ ! -f .post_upgrade_complete ]; then
     ./tools/post-upgrade.sh
   fi
@@ -23,18 +25,18 @@ fi
 
 run_app() {
   export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ ! -s "$NVM_DIR/nvm.sh" ] || \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
   sudo /sbin/ldconfig
 
   echo "nvm version"
-  nvm --version
+  nvm --version || echo "Use system's node insead of nvm"
   echo "node version"
   node --version
   echo "npm version"
   npm --version
   echo "Starting gateway ..."
-  npm start -- --check-wifi
+  npm start -- $args
 }
 
 mkdir -p "${MOZIOT_HOME}/log"
