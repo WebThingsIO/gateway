fe6e16a037799cbe8cdc5a004e7ab36babb4b557
Michael Stegeman <michael@stegeman.me>
2018-08-28 12:43:07 -0600

    Disallow ns* subdomains.
    Those are reserved for our nameservers.

--- static/js/setup_subdomain.js
+++ static/js/setup_subdomain.js
@@ -33,12 +33,16 @@ function displayMessage(errorMsg, type) {
  * Ensure that subdomain is valid:
  * - Contains only a-z, 0-9, and hyphens, but does not start or end with
  *   hyphen.
- * - Is not equal to "api" or "www", as those are reserved.
+ * - Is not equal to "api", "www", or "ns\d*", as those are reserved.
  */
 function validateDomain() {
   const val = subdomain.value.toLowerCase();
   const re = new RegExp(/^([a-z0-9]|[a-z0-9][a-z0-9-]*[a-z0-9])$/);
-  if (!re.test(val) || ['api', 'www'].includes(val) || val.length > 63) {
+  const nsRegex = new RegExp(/^ns\d*$/);
+  if (!re.test(val) ||
+      nsRegex.test(val) ||
+      ['api', 'www'].includes(val) ||
+      val.length > 63) {
     return false;
   }
 
