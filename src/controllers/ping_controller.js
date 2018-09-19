ac843f6fc3069101cb4af8fcebe6d1cd78605775
Michael Stegeman <michael@stegeman.me>
2018-09-19 07:27:01 -0600

    Inform user when connectivity to gateway drops.
    This doesn't handle the case where the gateway can't be reached
    when the user initially browses to the domain.
    
    References #727

---
+++ src/controllers/ping_controller.js
@@ -0,0 +1,21 @@
+/**
+ * Ping Controller.
+ *
+ * Handles requests to /ping, used for connectivity checks.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+'use strict';
+
+const express = require('express');
+
+const PingController = express.Router();
+
+PingController.get('/', (request, response) => {
+  response.status(200).json({status: 'ok'});
+});
+
+module.exports = PingController;
