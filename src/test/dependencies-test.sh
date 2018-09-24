26c39253451e4ad57636b9130580ff19719f4420
sogaani <soga.ani@gmail.com>
2018-09-25 00:06:15 +0900

    Add test to check dependencies

---
+++ src/test/dependencies-test.sh
@@ -0,0 +1,31 @@
+#! /bin/bash -e
+
+# This script will fail if dependencies in package.json is insufficient to run gateway.
+
+SCRIPTDIR=$(cd $(dirname $0);pwd)
+GATEWAYDIR=${SCRIPTDIR}/../../
+TMPDIR=/tmp/gateway-test
+GATEWAYSRC=${GATEWAYDIR}/build/gateway.js
+
+# Setup tempolary directory
+rm -rf ${TMPDIR}
+mkdir -p ${TMPDIR}
+cp ${GATEWAYDIR}/package.json ${TMPDIR}/
+cp ${GATEWAYDIR}/yarn.lock ${TMPDIR}/
+cd ${TMPDIR}
+
+# install only production dependencies
+yarn install --production
+
+# Correct modules used by Gateway.
+REQUIRE_MODULES=$(cat ${GATEWAYSRC} | sed -n -e 's/^.*\(require([^)]*)\).*$/\1/gp')
+
+trap onError ERR
+function onError() {
+  echo "dependencies in package.json is insufficient"
+  echo ""
+  echo "Add lacking modules to dependencies"
+}
+
+# Check modules through require all modules.
+node -e "${REQUIRE_MODULES}"
