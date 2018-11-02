c32ba67f81a7b05301ad904531583203fda7feba
Michael Stegeman <michael@stegeman.me>
2018-11-02 06:41:21 -0600

    Remove redundant NODE_TLS_REJECT_UNAUTHORIZED flags.

--- src/test/common.js
+++ src/test/common.js
@@ -9,7 +9,6 @@
 /* globals expect */
 
 process.env.NODE_ENV = 'test';
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const Database = require('../db');
 const Actions = require('../models/actions');
