c35dc82d658fa079dab13b96381550029b98d24a
Michael Stegeman <michael@stegeman.me>
2019-01-17 12:18:47 -0700

    Allow proxying of web thing content.
    Requires mozilla-iot/gateway-addon-node#26
    Fixes #1587

---
+++ src/controllers/proxy_controller.js
@@ -0,0 +1,46 @@
+/**
+ * Proxy Controller.
+ *
+ * Handles proxied resources.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+'use strict';
+
+const httpProxy = require('http-proxy');
+const Router = require('express-promise-router');
+
+const ProxyController = new Router();
+
+const proxies = new Map();
+const proxy = httpProxy.createProxyServer({
+  changeOrigin: true,
+});
+
+ProxyController.addProxyServer = (thingId, server) => {
+  proxies.set(thingId, server);
+};
+
+ProxyController.removeProxyServer = (thingId) => {
+  proxies.delete(thingId);
+};
+
+/**
+ * Log out the user
+ */
+ProxyController.all('/:thingId/*', (request, response) => {
+  const thingId = request.params.thingId;
+
+  if (!proxies.has(thingId)) {
+    response.sendStatus(404);
+    return;
+  }
+
+  request.url = request.url.substring(thingId.length + 1);
+  proxy.web(request, response, {target: proxies.get(thingId)});
+});
+
+module.exports = ProxyController;
