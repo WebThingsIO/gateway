242a98609cea105426c3713b36a5c75c1e9e4aaf
Michael Stegeman <michael@stegeman.me>
2019-02-12 14:01:43 -0700

    Pin gateway-addon library versions.

--- image/prepare-base.sh
+++ image/prepare-base.sh
@@ -63,7 +63,7 @@ sudo apt install -y \
   sqlite3
 
 # Install Python add-on bindings
-_url="git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon"
+_url="git+https://github.com/mozilla-iot/gateway-addon-python@v0.4.0#egg=gateway_addon"
 sudo pip2 install "$_url"
 sudo pip3 install "$_url"
 
