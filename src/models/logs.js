4b36afff87f40ffdbb5677817f240b2052a0c342
James Hobin <hobinjk@gmail.com>
2019-02-20 13:10:02 -0500

    Choose a database format

--- src/models/logs.js
+++ src/models/logs.js
@@ -4,20 +4,32 @@ const sqlite3 = require('sqlite3').verbose();
 
 const AddonManager = require('../addon-manager');
 const Constants = require('../constants');
-const Things = require('./things');
 const UserProfile = require('../user-profile');
+
+const METRICS_NUMBER = 'metricsNumber';
+const METRICS_BOOLEAN = 'metricsBoolean';
+const METRICS_OTHER = 'metricsOther';
 
 class Logs {
   constructor() {
     this.db = null;
     this.data = {};
+    this.idToDescr = {};
+    this.descrToId = {};
     this.onPropertyChanged = this.onPropertyChanged.bind(this);
 
     AddonManager.on(Constants.PROPERTY_CHANGED, this.onPropertyChanged);
   }
 
   clear() {
-    this.data = {};
+    return Promise.all([
+      METRICS_NUMBER,
+      METRICS_BOOLEAN,
+      METRICS_OTHER,
+      'metricIds',
+    ].map((table) => {
+      return this.run(`DELETE FROM ${table}`);
+    }));
   }
 
   open() {
@@ -27,8 +39,6 @@ class Logs {
       return;
     }
 
-    // Don't pull this from user-profile.js, because that would cause a
-    // circular dependency.
     let filename;
     let exists = false;
     if (process.env.NODE_ENV === 'test') {
@@ -50,65 +60,124 @@ class Logs {
 
     this.db.serialize(() => {
       this.createTables();
+      this.loadKnownMetrics();
     });
   }
 
   createTables() {
-    Things.getThings().then((things) => {
-      for (const thing of things.entries()) {
-        this.registerThing(thing);
-      }
+    return Promise.all([
+      this.createMetricTable(METRICS_NUMBER, typeof 0),
+      this.createMetricTable(METRICS_BOOLEAN, typeof false),
+      this.createMetricTable(METRICS_OTHER, typeof {}),
+      this.createIdTable(),
+    ]);
+  }
+
+  createIdTable() {
+    // We use a version of sqlite which doesn't support foreign keys so id is
+    // an integer referenced by the metric tables
+    return this.run(`CREATE TABLE IF NOT EXISTS metricIds (
+      id INTEGER PRIMARY KEY ASC,
+      descr TEXT
+    );`, []);
+  }
+
+  createMetricTable(id, type) {
+    const table = id;
+    let sqlType = 'TEXT';
+
+    switch (type) {
+      case 'number':
+        sqlType = 'REAL';
+        break;
+      case 'boolean':
+        sqlType = 'INTEGER';
+        break;
+    }
+
+    return this.run(`CREATE TABLE IF NOT EXISTS ${table} (
+      id INTEGER,
+      date DATE,
+      value ${sqlType}
+    );`, []);
+  }
+
+  async loadKnownMetrics() {
+    const rows = await this.all('SELECT id, descr FROM metricIds');
+    for (const row of rows) {
+      this.idToDescr[row.id] = row.descr;
+      this.descrToId[row.descr] = row.id;
+    }
+  }
+
+  propertyDescr(thingId, propId) {
+    return JSON.stringify({
+      type: 'property',
+      thing: thingId,
+      property: propId,
     });
   }
 
-  registerThing(thing) {
-    for (const name in thing.properties) {
-      const property = thing.properties[name];
-      this.createTable(this.propertyId(thing.id, name), property.type);
+  actionDescr(thingId, actionId) {
+    return JSON.stringify({
+      type: 'action',
+      thing: thingId,
+      action: actionId,
+    });
+  }
+
+  eventDescr(thingId, eventId) {
+    return JSON.stringify({
+      type: 'event',
+      thing: thingId,
+      event: eventId,
+    });
+  }
+
+  async registerMetric(descr) {
+    const result = await this.run(
+      'INSERT INTO metricIds (descr) VALUES (?)',
+      [descr]
+    );
+    const id = result.lastID;
+    this.idToDescr[id] = descr;
+    this.descrToId[descr] = id;
+    return id;
+  }
+
+  async insertMetric(descr, rawValue, date) {
+    let table = METRICS_OTHER;
+    let value = rawValue;
+
+    switch (typeof rawValue) {
+      case 'boolean':
+        table = METRICS_BOOLEAN;
+        break;
+      case 'number':
+        table = METRICS_NUMBER;
+        break;
+      default:
+        value = JSON.stringify(rawValue);
+        break;
     }
 
-    for (const name in thing.actions) {
-      this.createTable(this.actionId(thing.id, name), 'object');
+    let id = -1;
+    if (this.descrToId.hasOwnProperty(descr)) {
+      id = this.descrToId[descr];
+    } else {
+      id = await this.registerMetric(descr);
     }
 
-    for (const name in thing.events) {
-      this.createTable(this.eventId(thing.id, name), 'object');
-    }
-  }
-
-  propertyId(thingId, propId) {
-    return `thing_${thingId}_property_${propId}`;
-  }
-
-  actionId(thingId, actionId) {
-    return `thing_${thingId}_action_${actionId}`;
-  }
-
-  eventId(thingId, eventId) {
-    return `thing_${thingId}_event_${eventId}`;
-  }
-
-  createTable() {
-  }
-
-  migrate() {
+    await this.run(
+      `INSERT INTO ${table} (id, date, value) VALUES (?, ?, ?)`,
+      [id, date, value]
+    );
   }
 
   onPropertyChanged(property) {
     const thingId = property.device.id;
-    if (!this.data[thingId]) {
-      this.data[thingId] = {};
-    }
-
-    const thingData = this.data[thingId];
-    if (!thingData[property.name]) {
-      thingData[property.name] = [];
-    }
-
-    thingData[property.name].push({
-      value: property.value,
-      date: new Date(),
-    });
+    const descr = this.propertyDescr(thingId, property.name);
+    this.insertMetric(descr, property.value, new Date());
   }
 
   onEvent() {
@@ -117,30 +186,43 @@ class Logs {
   onAction() {
   }
 
-  async getAll() {
-    // const out = {};
-    // const rows = await this.all('SELECT id, value, date FROM metricsNumber');
+  async loadMetrics(out, table, transformer) {
+    const rows = await this.all(`SELECT id, value, date FROM ${table}`);
+    console.log('loadMetrics', this.idToDescr, rows);
+    for (const row of rows) {
+      const descr = JSON.parse(this.idToDescr[row.id]);
+      if (!out.hasOwnProperty(descr.thing)) {
+        out[descr.thing] = {};
+      }
+      if (!out[descr.thing].hasOwnProperty(descr.property)) {
+        out[descr.thing][descr.property] = [];
+      }
+      const value = transformer ? transformer(row.value) : row.value;
+      out[descr.thing][descr.property].push({
+        value: value,
+        date: row.date,
+      });
+    }
+  }
 
-    // for (const row of rows) {
-    //   const id = this.foreignKeys[row.id];
-    //   if (!out.hasOwnProperty(id)) {
-    //     out[id] = [];
-    //   }
-    //   out[id].push({
-    //     value: row.value,
-    //     date: row.date,
-    //   });
-    // }
-    // return out;
-    return this.data;
+
+  async getAll() {
+    const out = {};
+    await this.loadMetrics(out, METRICS_NUMBER);
+    await this.loadMetrics(out, METRICS_BOOLEAN, (value) => !!value);
+    await this.loadMetrics(out, METRICS_OTHER, (value) => JSON.parse(value));
+    console.log('getAll', out);
+    return out;
   }
 
   async get(thingId) {
-    return this.data[thingId];
+    const all = await this.getAll();
+    return all[thingId];
   }
 
   async getProperty(thingId, propertyName) {
-    return this.data[thingId][propertyName];
+    const all = await this.getAll();
+    return all[thingId][propertyName];
   }
 
   all(sql, ...params) {
@@ -155,6 +237,29 @@ class Logs {
 
       try {
         this.db.all(sql, ...params);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  /**
+   * Run a SQL statement
+   * @param {String} sql
+   * @param {Array<any>} values
+   * @return {Promise<Object>} promise resolved to `this` of statement result
+   */
+  run(sql, values) {
+    return new Promise((accept, reject) => {
+      try {
+        this.db.run(sql, values, function(err) {
+          if (err) {
+            reject(err);
+            return;
+          }
+          // node-sqlite puts results on "this" so avoid arrrow fn.
+          accept(this);
+        });
       } catch (err) {
         reject(err);
       }
