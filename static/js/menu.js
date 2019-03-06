edcfbe3f139ca1162664be7c4017191801b438fc
kale-io <cos+a@iuvo.ai>
2019-03-06 23:12:39 +0000

    Custom Rule

--- static/js/menu.js
+++ static/js/menu.js
@@ -40,7 +40,7 @@ const Menu = {
    * experiment is enabled
    * @param {String} experiment
    */
-  getExperimentSetting: experiment => {
+  getExperimentSetting: (experiment) => {
     API.getExperimentSetting(experiment).then((value) => {
       if (value) {
         Menu.showItem(experiment);
