047f5fda12bc41283825617c8a8fced7f01d4343
James Hobin <hobinjk@gmail.com>
2019-03-28 17:45:23 -0400

    Give static/js/logs/log.js a proper preamble

--- static/js/logs/log.js
+++ static/js/logs/log.js
@@ -1,4 +1,15 @@
-// const Thing = require('../thing');
+/**
+ * Log
+ *
+ * Draws a single log
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+'use strict';
+
 const App = require('../app');
 const Utils = require('../utils');
 
