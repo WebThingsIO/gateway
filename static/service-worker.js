831f5a023474a2a0aff1df8a151f73313618daa6
Michael Stegeman <michael@stegeman.me>
2019-04-04 12:13:17 -0600

    Fix issue where browser tries to download log files. (#1739)

--- static/service-worker.js
+++ static/service-worker.js
@@ -26,8 +26,10 @@ self.addEventListener('fetch', (event) => {
   }
 
   const url = new URL(event.request.url);
-  if (url.origin !== location.origin || url.pathname.endsWith('.map') ||
-      url.pathname.endsWith('floorplan.svg')) {
+  if (url.origin !== location.origin ||
+      url.pathname.endsWith('.map') ||
+      url.pathname.endsWith('floorplan.svg') ||
+      url.pathname.startsWith('/logs/files/')) {
     return;
   }
 
