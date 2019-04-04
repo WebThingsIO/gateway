4af07ebdadadc2a6eba771d06b172553e1baeaf8
Michael Stegeman <michael@stegeman.me>
2019-04-04 08:31:16 -0600

    Split out config files from image scripts.
    Fixes #1510

--- image/prepare-base.sh
+++ image/prepare-base.sh
@@ -75,162 +75,24 @@ sudo setcap cap_net_raw+eip $(eval readlink -f `which python3`)
 
 git clone https://github.com/mozilla-iot/intent-parser "$HOME/mozilla-iot/intent-parser"
 
-# Create the service file needed by systemctl
-sudo sh -c 'cat > /etc/systemd/system/mozilla-iot-gateway.service' <<END
-[Unit]
-Description=Mozilla WebThings Gateway Client
-After=network.target
-OnFailure=mozilla-iot-gateway.update-rollback.service
+# Create the service files needed by systemd
+sudo chown -R root:root ./etc
+sudo cp -r ./etc /
 
-[Service]
-Type=simple
-StandardOutput=journal
-StandardError=journal
-# Edit this line, if needed, to specify what user to run Vaani as
-# If you delete this line, it will run as root
-User=pi
-# Edit this line, if needed, to specify where you installed the server
-WorkingDirectory=/home/pi/mozilla-iot/gateway
-# Edit this line, if needed, to set the correct path to node
-ExecStart=/home/pi/mozilla-iot/gateway/run-app.sh
-Restart=always
-RestartSec=10s
-
-[Install]
-WantedBy=multi-user.target
-END
-
-sudo sh -c 'cat > /etc/systemd/system/mozilla-iot-gateway.update-rollback.service' <<END
-[Unit]
-Description=Mozilla WebThings Gateway Client Update Rollback
-After=network.target
-
-[Service]
-Type=simple
-StandardOutput=journal
-StandardError=journal
-User=pi
-# Edit this line, if needed, to specify where you installed the server
-WorkingDirectory=/home/pi/mozilla-iot/
-# Edit this line, if needed, to set the correct path to the script
-ExecStart=/home/pi/mozilla-iot/gateway/tools/rollback.sh
-
-END
-
-sudo sh -c 'cat > /etc/systemd/system/mozilla-iot-gateway.check-for-update.service' <<END
-[Unit]
-Description=Mozilla WebThings Gateway Client Update Checker
-After=network.target
-
-[Service]
-Type=simple
-StandardOutput=journal
-StandardError=journal
-User=pi
-# Edit this line, if needed, to specify where you installed the server
-WorkingDirectory=/home/pi/mozilla-iot/gateway
-# Edit this line, if needed, to set the correct path to the script
-ExecStart=/home/pi/mozilla-iot/gateway/tools/check-for-update.sh
-
-END
-
-sudo sh -c 'cat > /etc/systemd/system/mozilla-iot-gateway.check-for-update.timer' <<END
-[Unit]
-Description=Run the Mozilla WebThings Gateway update checker daily
-
-[Timer]
-OnCalendar=daily
-
-[Install]
-WantedBy=timers.target
-END
+# disable hostapd and dnsmasq auto start
+sudo systemctl disable hostapd.service
+sudo systemctl disable dnsmasq.service
 
 # Enable the gateway service so that it starts up automatically on each boot
 sudo systemctl enable mozilla-iot-gateway.service
 
-# Install required root packages
-sudo ./prepare-base-root.sh
-
 # Check for an update every day.
 sudo systemctl enable mozilla-iot-gateway.check-for-update.timer
 
-sudo sh -c 'cat > /etc/systemd/system/mozilla-iot-gateway.intent-parser.service' <<END
-[Unit]
-Description=Mozilla WebThings Gateway Intent Parser
-After=network.target
-OnFailure=mozilla-iot-gateway.update-rollback.service
-
-[Service]
-Type=simple
-StandardOutput=journal
-StandardError=journal
-User=pi
-# Edit this line, if needed, to specify where you installed the server
-WorkingDirectory=/home/pi/mozilla-iot/intent-parser
-# Edit this line, if needed, to set the correct path to node
-ExecStart=/home/pi/mozilla-iot/intent-parser/intent-parser-server.py
-Restart=always
-RestartSec=10s
-
-[Install]
-WantedBy=multi-user.target
-END
-
+# Enable the intent parser at boot
 sudo systemctl enable mozilla-iot-gateway.intent-parser.service
 
-# Create a script which redirects port 80 to 8080 and 443 to 4443. This
-# allows the gateway to run under the pi user rather than requiring to
-# be root.
-
-sudo sh -c 'cat > /etc/init.d/gateway-iptables.sh' << 'END'
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides:          gateway-iptables
-# Required-Start:    $all
-# Required-Stop:
-# Default-Start:     3 4 5
-# Default-Stop:
-# Short-Description: Redirect :80 to :8080 and :443 to :4443
-### END INIT INFO
-
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
-
-. /lib/init/vars.sh
-. /lib/lsb/init-functions
-
-do_start() {
-  [ "$VERBOSE" != no ] && log_begin_msg "Redirecting :80 to :8080 and :443 to :4443"
-  iptables -t mangle -A PREROUTING -p tcp --dport 80 -j MARK --set-mark 1
-  iptables -t mangle -A PREROUTING -p tcp --dport 443 -j MARK --set-mark 1
-  iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
-  iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 4443
-  iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -m mark --mark 1 -j ACCEPT
-  iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 4443 -m mark --mark 1 -j ACCEPT
-  ES=$?
-  [ "$VERBOSE" != no ] && log_end_msg $ES
-  return $ES
-}
-
-case "$1" in
-    start)
-	do_start
-        ;;
-    restart|reload|force-reload)
-        echo "Error: argument '$1' not supported" >&2
-        exit 3
-        ;;
-    stop|status)
-        # No-op
-        exit 0
-        ;;
-    *)
-        echo "Usage: $0 start|stop" >&2
-        exit 3
-        ;;
-esac
-END
-
-# Activate the script so that it runs on each boot.
+# Activate the iptables script so that it runs on each boot.
 #
 # NOTE: Do NOT be tempted to merge this with the systemctl stuff.
 #       There are 2 reasons for this. The first is that run-app.sh
@@ -242,10 +104,9 @@ END
 #       could stop and restart the gateway and if this was merged
 #       with run-app.sh then you'd be installing the rules a second
 #       time.
-sudo chmod +x /etc/init.d/gateway-iptables.sh
-sudo update-rc.d gateway-iptables.sh defaults
+sudo update-rc.d gateway-iptables defaults
 
-# Clean up the preparation scripts.
-sudo rm /home/pi/prepare-base{,-root}.sh
+# Clean up the preparation script and config files.
+sudo rm /home/pi/prepare-base.sh /home/pi/etc
 
 echo "Done"
