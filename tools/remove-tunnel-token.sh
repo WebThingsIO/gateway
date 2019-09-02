4253e753862ce3c76304a798e79ce293593aa182
Abhijit Nathwani <abhijit.nathwani@gmail.com>
2019-09-02 23:42:16 +0530

    Merged the scripts for tunnel re-registration

--- tools/remove-tunnel-token.sh
+++ tools/remove-tunnel-token.sh
@@ -2,5 +2,6 @@
 
 MOZIOT_HOME="${MOZIOT_HOME:=${HOME}/.mozilla-iot}"
 sqlite3 "${MOZIOT_HOME}/config/db.sqlite3" "DELETE FROM settings WHERE key='tunneltoken';"
+sqlite3 "${MOZIOT_HOME}/config/db.sqlite3" "DELETE FROM settings WHERE key='notunnel';"
 rm -rf "${MOZIOT_HOME}/ssl/"
 sudo service mozilla-iot-gateway restart
