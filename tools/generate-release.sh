8d22b211f9c9a21db3dca630f74fca5e904bc570
Dave Hylands <dhylands@gmail.com>
2019-10-23 14:09:02 -0700

    Update generate-release.sh script to use rpi-image-builder tarball (#2235)
    * Update generate-release.sh script to use rpi-image-builder tarball
    
    Also updated the wiki at:
    https://github.com/mozilla-iot/wiki/wiki/How-To-Release-a-Gateway-OTA-Update
    
    * Address review comments

--- tools/generate-release.sh
+++ tools/generate-release.sh
@@ -1,29 +1,70 @@
-#!/bin/bash
+#!/bin/bash -e
+#
+# Takes the gateway archive (created by rpi-image-builder) and creates
+# release archives.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Generates release archives based on the current Gateway revision
+SCRIPT_NAME=$(basename $0)
+SCRIPT_DIR=$(dirname $0)
+VERBOSE=0
+OUTPUT_DIRECTORY='ota'
 
-# Expects to be run in the Gateway directory and has the potential for errors
-# if package.json has uncommitted changes.
+###########################################################################
+#
+# Prints the program usage
+#
+usage() {
+  echo "Usage: ${SCRIPT_NAME} [-v] [-g gateway-tarball] [-o output-directory]"
+}
 
-if [ ! -e "package.json" ]; then
-  echo "Usage: ./tools/generate-release.sh"
-  echo "Error: missing package.json"
-  exit 1
-fi
+###########################################################################
+#
+# Parses command line arguments and runs the program.
+#
+main() {
 
-git init .
-git config user.email "temporary@example.com"
-git config user.name "Temporary"
-git add .
-git commit -m "Temporary"
-git clone ./ gateway
-cd gateway || exit 1
-rm -fr .git
-cp -r ../node_modules ./
-npm install
-./node_modules/.bin/webpack
-rm -fr ./node_modules
-cd .. || exit 1
-tar czf gateway.tar.gz gateway
-rm -fr gateway
-./tools/create-release-archives.sh
+  while getopts "g:ho:v" opt "$@"; do
+    case $opt in
+      g)
+        GATEWAY_TARBALL=${OPTARG}
+        ;;
+      o)
+        OUTPUT_DIRECTORY=${OPTARG}
+        ;;
+      v)
+        VERBOSE=1
+        ;;
+      h)
+        usage
+        exit 1
+        ;;
+      ?)
+        echo "Unrecognized option: ${opt}"
+        usage
+        exit 1
+        ;;
+      esac
+  done
+  if [ "${VERBOSE}" == "1" ]; then
+    echo " Gateway tarball: ${GATEWAY_TARBALL}"
+    echo "Output Directory: ${OUTPUT_DIRECTORY}"
+  fi
+
+  # The create-release-archives wants a gateway.tar.gz tarball
+  # (which has no node_modules in it) and a separate directory tree
+  # with all of the node_modules in it.
+  rm -rf "${OUTPUT_DIRECTORY}"
+  mkdir -p "${OUTPUT_DIRECTORY}"
+  tar -C "${OUTPUT_DIRECTORY}" -xf "${GATEWAY_TARBALL}"
+  mv "${OUTPUT_DIRECTORY}/gateway/node_modules" "${OUTPUT_DIRECTORY}/node_modules"
+  tar -C "${OUTPUT_DIRECTORY}" -czf "${OUTPUT_DIRECTORY}/gateway.tar.gz" gateway
+  rm -rf "${OUTPUT_DIRECTORY}/gateway"
+  (cd "${OUTPUT_DIRECTORY}"; ${SCRIPT_DIR}/create-release-archives.sh)
+  rm -rf "${OUTPUT_DIRECTORY}/node_modules"
+  ls -l "${OUTPUT_DIRECTORY}"
+}
+
+main "$@"
