afd7fd6d8cadab667c6ace81d63131c93994c635
Michael Stegeman <michael@stegeman.me>
2019-12-04 07:22:00 -0900

    Add minimal Arch Linux platform file. (#2331)
    Also, strip out Raspbian-specific functionality from Debian.

--- src/platforms/linux-debian.js
+++ src/platforms/linux-debian.js
@@ -8,4 +8,33 @@
 
 'use strict';
 
-module.exports = require('./linux-raspbian');
+const Raspbian = require('./linux-raspbian');
+
+module.exports = {
+  getDhcpServerStatus: Raspbian.getDhcpServerStatus,
+  setDhcpServerStatus: Raspbian.setDhcpServerStatus,
+  getHostname: Raspbian.getHostname,
+  setHostname: Raspbian.setHostname,
+  getLanMode: Raspbian.getLanMode,
+  setLanMode: Raspbian.setLanMode,
+  getMacAddress: Raspbian.getMacAddress,
+  // getSshServerStatus,
+  // setSshServerStatus,
+  getMdnsServerStatus: Raspbian.getMdnsServerStatus,
+  setMdnsServerStatus: Raspbian.setMdnsServerStatus,
+  getNetworkAddresses: Raspbian.getNetworkAddresses,
+  getWirelessMode: Raspbian.getWirelessMode,
+  setWirelessMode: Raspbian.setWirelessMode,
+  restartGateway: Raspbian.restartGateway,
+  restartSystem: Raspbian.restartSystem,
+  scanWirelessNetworks: Raspbian.scanWirelessNetworks,
+  update: Raspbian.update,
+  getValidTimezones: Raspbian.getValidTimezones,
+  getTimezone: Raspbian.getTimezone,
+  // setTimezone,
+  getValidWirelessCountries: Raspbian.getValidWirelessCountries,
+  // getWirelessCountry,
+  // setWirelessCountry,
+  getNtpStatus: Raspbian.getNtpStatus,
+  restartNtpSync: Raspbian.restartNtpSync,
+};
