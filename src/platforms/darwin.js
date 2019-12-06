61e23b9f05029475c5d83b895e8297fba661579c
Michael Stegeman <michael@stegeman.me>
2019-12-06 06:36:10 -0900

    Add basic Darwin platform utilities. (#2348)
    There are a lot more things which are possible, but most require
    sudo access.

---
+++ src/platforms/darwin.js
@@ -0,0 +1,147 @@
+/**
+ * Darwin (macOS) platform interface.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+'use strict';
+
+const child_process = require('child_process');
+const fs = require('fs');
+
+/**
+ * Get mDNS server status.
+ *
+ * @returns {boolean} Boolean indicating whether or not mDNS is enabled.
+ */
+function getMdnsServerStatus() {
+  // mDNS is always enabled
+  return true;
+}
+
+/**
+ * Set mDNS server status.
+ *
+ * @param {boolean} enabled - Whether or not to enable the mDNS server
+ * @returns {boolean} Boolean indicating success of the command.
+ */
+function setMdnsServerStatus(enabled) {
+  if (enabled) {
+    return true;
+  }
+
+  // can't disable
+  return false;
+}
+
+/**
+ * Get the system's hostname.
+ *
+ * @returns {string} The hostname.
+ */
+function getHostname() {
+  const proc = child_process.spawnSync('hostname', {encoding: 'utf8'});
+  if (proc.status !== 0) {
+    return '';
+  }
+
+  return proc.stdout.trim();
+}
+
+/**
+ * Update the gateway and the system.
+ *
+ * @returns {Promise} Promise which resolves when the update is complete.
+ */
+function update() {
+  // TODO: replace external update mechanism with this function, if possible
+  return Promise.resolve({
+    rebootRequired: false,
+    gatewayRestartRequired: false,
+  });
+}
+
+/**
+ * Get a list of all valid timezones for the system.
+ *
+ * @returns {string[]} List of timezones.
+ */
+function getValidTimezones() {
+  const tzdata = '/usr/share/zoneinfo/zone.tab';
+  if (!fs.existsSync(tzdata)) {
+    return [];
+  }
+
+  try {
+    const data = fs.readFileSync(tzdata, 'utf8');
+    const zones = data.split('\n')
+      .filter((l) => !l.startsWith('#') && l.length > 0)
+      .map((l) => l.split(/\s+/g)[2])
+      .sort();
+
+    return zones;
+  } catch (e) {
+    console.error('Failed to read zone file:', e);
+  }
+
+  return [];
+}
+
+/**
+ * Get the current timezone.
+ *
+ * @returns {string} Name of timezone.
+ */
+function getTimezone() {
+  const tzdata = '/etc/localtime';
+  if (!fs.existsSync(tzdata)) {
+    return '';
+  }
+
+  try {
+    const data = fs.readlinkSync(tzdata);
+    return data.substring(data.indexOf('zoneinfo/') + 'zoneinfo/'.length);
+  } catch (e) {
+    console.error('Failed to read timezone:', e);
+  }
+
+  return '';
+}
+
+/**
+ * Get a list of all valid wi-fi countries for the system.
+ *
+ * @returns {string[]} List of countries.
+ */
+function getValidWirelessCountries() {
+  const fname = '/usr/share/zoneinfo/iso3166.tab';
+  if (!fs.existsSync(fname)) {
+    return [];
+  }
+
+  try {
+    const data = fs.readFileSync(fname, 'utf8');
+    const zones = data.split('\n')
+      .filter((l) => !l.startsWith('#') && l.length > 0)
+      .map((l) => l.split('\t')[1])
+      .sort();
+
+    return zones;
+  } catch (e) {
+    console.error('Failed to read zone file:', e);
+  }
+
+  return [];
+}
+
+module.exports = {
+  getHostname,
+  getMdnsServerStatus,
+  setMdnsServerStatus,
+  update,
+  getValidTimezones,
+  getTimezone,
+  getValidWirelessCountries,
+};
