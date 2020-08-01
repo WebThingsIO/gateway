84a2ac210edd8a1e135b5d0651bcd0adb1c2abae
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-08-01 22:45:31 +0200

    Fix typo

--- static/js/schema-impl/capability/humidity-sensor.js
+++ static/js/schema-impl/capability/humidity-sensor.js
@@ -1,7 +1,7 @@
 /**
  * Humidity sensor.
  *
- * UI element representing a umidity sensor.
+ * UI element representing a humidity sensor.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
