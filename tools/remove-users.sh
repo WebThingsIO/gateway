75e1e7e337d02df3e5aef0c164d92362c35106e2
Michael Stegeman <michael@stegeman.me>
2020-11-30 11:20:28 -0900

    Stop using `service` in scripts.

--- tools/remove-users.sh
+++ tools/remove-users.sh
@@ -2,4 +2,4 @@
 
 WEBTHINGS_HOME="${WEBTHINGS_HOME:=${HOME}/.webthings}"
 sqlite3 "${WEBTHINGS_HOME}/config/db.sqlite3" "DELETE FROM users"
-sudo service webthings-gateway restart
+sudo systemctl restart webthings-gateway
