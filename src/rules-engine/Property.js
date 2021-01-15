76cb78bbe9084b37fa88c4103ecbc1e6b061a2f5
Michael Stegeman <michael@stegeman.me>
2021-01-15 09:10:07 -0900

    Clean up event emitters. (#2713)

--- src/rules-engine/Property.js
+++ src/rules-engine/Property.js
@@ -10,7 +10,7 @@ const assert = require('assert');
 const AddonManager = require('../addon-manager');
 const Constants = require('../constants');
 const Things = require('../models/things');
-const EventEmitter = require('events').EventEmitter;
+const {EventEmitter} = require('events');
 const Events = require('./Events');
 
 /**
