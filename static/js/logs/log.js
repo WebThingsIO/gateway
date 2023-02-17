23d36787807d8bfde9c1130294fc18f867b5fa9d
flatsiedatsie <unfold@gmail.com>
2023-02-17 14:13:50 +0100

    Bugfix for NaN issue (#3072)

--- static/js/logs/log.js
+++ static/js/logs/log.js
@@ -206,10 +206,10 @@ class Log {
     }
     const onChange = () => {
       let newEnd = this.end.getTime();
-      let newStart = newEnd - this.weekDropdown.value;
+      let newStart = newEnd - parseInt(this.weekDropdown.value);
       if (newStart < this.logStart.getTime()) {
         newStart = this.logStart.getTime();
-        newEnd = newStart + this.weekDropdown.value;
+        newEnd = newStart + parseInt(this.weekDropdown.value);
       }
       this.start = new Date(newStart);
       this.end = new Date(newEnd);
