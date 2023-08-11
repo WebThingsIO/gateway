09ce04463660bf00c8e6663cda7ce922b95c5ba4
Ben Francis <ben@tola.me.uk>
2023-08-11 21:12:09 +0100

    Disable browser tests until they can be fixed

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
   testPathIgnorePatterns: [
     '/node_modules/',
     '/src/',
+    '<rootDir>/test/browser/', // https://github.com/WebThingsIO/gateway/issues/3007
   ],
   transform: {
     '\\.js$': 'babel-jest',
